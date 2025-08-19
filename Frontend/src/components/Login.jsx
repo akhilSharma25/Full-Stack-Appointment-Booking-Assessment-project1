d1c4f05aa0101be3311609b63ab7a72c6bfbf654
Akhil Sharma <139208480+akhilSharma25@users.noreply.github.com>
2025-08-20 03:40:15 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ export default function Login({ setUser }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:8000/api/login", {
+      const res = await fetch("https://full-stack-appointment-booking.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
